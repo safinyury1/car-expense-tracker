f14e0daa68dcfaf17adfea6558c1e3ef6e2a3852
safinyury1 <safin15092005@mail.ru>
2026-04-03 01:33:06 +0500

    breeze,migration

---
+++ database/migrations/2026_04_02_201826_create_cars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('brand');
+            $table->string('model');
+            $table->year('year')->nullable();
+            $table->string('vin', 17)->nullable();
+            $table->integer('initial_odometer')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
